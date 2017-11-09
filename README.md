c3cc7ad8819fed4b4f377e4fe02b648159e1927e
lftomazini <lftomazini@hotmail.com>
2017-11-09 15:38:17 -0200

    Final documentation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hilzersBarbershop
 Implementation of the Hilzer's Barbershop problem to control deadlocks
+
+## Compile
+make
+
+## Run
+./deadlockControl <number of chairs> <number of barbers> <number of seats> <maximum nunmber of costumers>
--- deadlockControl.c
+++ deadlockControl.c
@@ -6,19 +6,31 @@
 #include <unistd.h>
 #include "queue.h"
 
+/*
+ * Variables to indicate the number of instances per resource
+*/
 int num_clients = 0;
-int tranks_clients = 0;
-//int num_payment = 0;
 int num_chairs = 3;
 int num_barbers = 3;
 int num_seats_sofa = 4;
 int num_max_costumers = 20;
 
+
+/*
+ * Threads to run costumers' and barbers' actions
+*/
 pthread_t *thr_costumers;
 pthread_t *thr_barbers;
 
+
+/*
+ * Queue for the costumers in the waiting room and for the sofa
+*/
 struct Queue *q_waiting_room, *q_sofa; 
 
+/*
+ * Semaphores for each one of the resources and one master key (mutex)
+*/
 sem_t mutex;
 sem_t queue_wr;
 sem_t queue_s;
@@ -29,34 +41,36 @@ sem_t costumer;
 sem_t payment;
 sem_t cash_register;
 
-// sem_t waiting_room[2], sofa[2];
 sem_t waiting_room_empty, sofa_empty;
 
 char* barbers [13] = {"Jerry Smith", "Nando DK", "Jhowzinho", "Kadinho", "Livinho", "Kevinho", "G15", "Gustta", "Lucas Lucco", "Yuri Martins", "Lan", "WM", "Don Juan"};
 
-
-
-// void print_initial_instances(resource resource);
-// int num_allocated_instances(resource resource);
-// int num_instances_left_to_be_requested(resource resource);
-// int num_instances_available(resource resource);
-// void insecure_state_blocked_processes(resource resource);
-
-
+/*
+ * Display a message to show that the barbershop is full
+*/
 void shop_is_full(int clientID) {
 	printf("C%d: X The shop is full. No more costumers are allowed to enter. Client is leaving the store.\n", clientID);
 }
 
+/*
+ * Display a message that the barber started to cut the hair
+*/
 void cutting_hair(int barberID) {
 	printf("Barber %s is going to cut the hair.\n", barbers[barberID]);
 	sleep(rand()%6+4);
 }
 
+/*
+ * Display a message that the barber will receive the payment
+*/
 void receiving_payment(int barberID) {
 	printf("Barber %s is receiving the payment.\n", barbers[barberID]);
 	sleep(rand()%4);
 }
 
+/*
+ * Display a message that the costumer is going to the waiting room
+*/
 void going_to_waiting_room(int clientID) {
 	if(isFull(q_sofa)){
 		printf("C%d: 0 The client is entering the waiting room.\n", clientID);
@@ -65,27 +79,46 @@ void going_to_waiting_room(int clientID) {
 	sleep(1);
 }
 
+/*
+ * Display a message that the costumer is going to the seat on the sofa
+*/
 void going_to_the_sofa(int clientID) {
 	printf("C%d: 1 The client is seating on the sofa.\n", clientID);
 	sleep(rand()%2);
 }
 
+/*
+ * Display a message that the costumer is going to receive a haircut
+*/
 void going_to_the_chair(int clientID) {
 	printf("C%d: 2 Ah, ah, aah. Now you gonna seat!.\n", clientID);
 	sleep(rand()%2);
 }
 
+/*
+ * Display a message that the costumer finished to have the haircut
+*/
 void got_hair_cut(int clientID) {
 	printf("C%d: 3 The client is now avonts and is going to pay the bill.\n", clientID);
 	sleep(rand()%3);
 }
 
+/*
+ * Display a message that the costumer is ready to leave the shop
+*/
 void client_is_leaving(int clientID) {
 	printf("C%d: 4 The client is tranks and is leaving the shop.\n", clientID);
-    ++tranks_clients;
 	sleep(rand()%3);
 }
 
+/*
+ * Executes the costumer's actions.
+ * Mutex is used when the number of costumers needs to be changed.
+ * Other semaphores are used to access other non critical resources, such as:
+ * for the sofa, chairs, barbers and costumers.
+ *
+ * void *v: is the costumer ID
+*/
 void* f_costumer (void *v) {
 	int clientID = *(int *) v;
 	sem_wait(&mutex);
@@ -95,46 +128,28 @@ void* f_costumer (void *v) {
 	}
 	++num_clients;
 	sem_post(&mutex);
-
-
 	sem_wait(&waiting_room_empty);
-	//sem_wait(&queue_wr);
 	sem_wait(&queue_wr);
 	enqueue(q_waiting_room,clientID);
 	going_to_waiting_room(clientID);
 	sem_post(&queue_wr);
-	//down_queue(&waiting_room_full, &waiting_room_empty);
-	
-//	sem_post(&queue_wr);
-	//sem_post(&queue_wr);
 	sem_wait(&sofa_empty);
-	//sem_wait(&queue_wr);
 	sem_wait(&queue_s);
 	clientID = dequeue(q_waiting_room);
 	sem_post(&waiting_room_empty);
 	enqueue(q_sofa,clientID);
-    //down_queue(&sofa_full, &sofa_empty);
 	going_to_the_sofa(clientID);
 	sem_post(&queue_s);
-	
-		//up_queue(&waiting_room_full, &waiting_room_empty);
-		//sem_post(&queue_wr);
-	//}
-
-
     sem_wait(&chair);
     sem_wait(&queue_c);
 	clientID = dequeue(q_sofa);
 	going_to_the_chair(clientID);
 	sem_post(&queue_c);
 	sem_post(&sofa_empty);
-	//up_queue(&sofa_full, &sofa_empty);
-    //sem_post(&queue);
 	sem_post(&costumer);
 	sem_wait(&barber);
 	got_hair_cut(clientID);
 	sem_post(&payment);
-	//sem_post(&mutex);
 	sem_wait(&cash_register);
 	sem_wait(&mutex);
 	--num_clients;
@@ -142,6 +157,14 @@ void* f_costumer (void *v) {
 	client_is_leaving(clientID);
 }
 
+/*
+ * Executes the barber's actions, while there are still costumers.
+ * Actions include decrease the number of costumers that need to receive a haircut,
+ * increase the number of barbers working, increase the number of chairs occupied,
+ * receive the payment and use the cash register.
+ *
+ * void *v: is the barber ID
+*/
 void* f_barber (void *v) {
 	int barberID = *(int *) v;
 	
@@ -150,14 +173,9 @@ void* f_barber (void *v) {
 		sem_post(&barber);
 		cutting_hair(barberID);
 		sem_post(&chair);
-		//sem_wait(&payment);
-		//while(num_payment>0)
 		sem_wait(&payment);
 		receiving_payment(barberID);
 		sem_post(&cash_register);
-
-		
-		
 	}
 }
 
@@ -166,30 +184,32 @@ void* f_barber (void *v) {
  */
 int check_arguments(int argc, char* argv[]) {
 	if (argc != 5) {
-		//printf("Usage: ./<number of processes> <number of ressources> <number of instances per ressource>\n");
+		printf("Usage: %s <number of chairs> <number of barbers> <number of seats> <maximum nunmber of costumers>\n", argv[0]);
 
-		//exit(0);
+		exit(0);
 		return 0;
 	}
 	return 1;
 }
 
-
+/*
+ * Runs the barbershop. Checks the arguments, initialize the semaphores, creates the costumers and barbers.
+*/
 int main(int argc, char* argv[]) {
-	if(check_arguments(argc, argv)){
+	if(check_arguments(argc, argv)) {
 
 		num_chairs = atoi(argv[1]);
 		num_barbers = atoi(argv[2]);
 		num_seats_sofa = atoi(argv[3]);
 		num_max_costumers = atoi(argv[4]);
-	}	
+	}
+
     srand( (unsigned)time(NULL) );
 	thr_costumers = (pthread_t *)malloc(sizeof(pthread_t) * num_max_costumers);
 	thr_barbers = (pthread_t *)malloc(sizeof(pthread_t) * num_barbers);
 	
 	q_waiting_room = createQueue(num_max_costumers-num_seats_sofa);
 	q_sofa = createQueue(num_seats_sofa);
-	//q_payment = createQueue(num_max_costumers);
 
 	int client[num_max_costumers];
 
@@ -207,9 +227,8 @@ int main(int argc, char* argv[]) {
 
 	int i = 0;
 
-    int al = rand()%10;
+    int al = rand() % 10;
     int shuffle[num_barbers];
-
 
 	for (i = 0; i < num_max_costumers; i++) {
 		client[i] = i;
@@ -218,15 +237,13 @@ int main(int argc, char* argv[]) {
 	}
 
     for (i = 0; i < num_barbers; i++) {
-        shuffle[i]= al+i;
+        shuffle[i]= al + i;
 		pthread_create(&thr_barbers[i], 0, f_barber, &shuffle[i]);
 	}
-    //sleep(11000);
-    for (i = 0; i < num_max_costumers; i++) {
 
+    for (i = 0; i < num_max_costumers; i++) {
 		pthread_join(thr_costumers[i], NULL);
 	}
-	
 
 	free(thr_costumers);
 	free(thr_barbers);
