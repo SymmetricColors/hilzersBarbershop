4465e68ab682b9efd3a58b6f1df783f16c980f17
Caio <caiosilva96@hotmail.com>
2017-11-09 13:44:15 -0200

    Com o bumbum ela balança

--- deadlockControl.c
+++ deadlockControl.c
@@ -12,7 +12,7 @@ int tranks_clients = 0;
 int num_chairs = 3;
 int num_barbers = 3;
 int num_seats_sofa = 4;
-int num_total_customers = 20;
+int num_max_costumers = 20;
 
 pthread_t *thr_costumers;
 pthread_t *thr_barbers;
@@ -30,7 +30,7 @@ sem_t payment;
 sem_t cash_register;
 
 // sem_t waiting_room[2], sofa[2];
-sem_t waiting_room_empty, waiting_room_full, sofa_empty, sofa_full;
+sem_t waiting_room_empty, sofa_empty;
 
 char* barbers [13] = {"Jerry Smith", "Nando DK", "Jhowzinho", "Kadinho", "Livinho", "Kevinho", "G15", "Gustta", "Lucas Lucco", "Yuri Martins", "Lan", "WM", "Don Juan"};
 
@@ -42,17 +42,6 @@ char* barbers [13] = {"Jerry Smith", "Nando DK", "Jhowzinho", "Kadinho", "Livinh
 // int num_instances_available(resource resource);
 // void insecure_state_blocked_processes(resource resource);
 
-void check_arguments(int argc, char* argv[]);
-
-void up_queue (sem_t *full, sem_t *empty) {
-	sem_wait(full);
-	sem_post(empty);
-}
-
-void down_queue (sem_t *full, sem_t *empty) {
-	sem_wait(empty);
-	sem_post(full);
-}
 
 void shop_is_full(int clientID) {
 	printf("C%d: X The shop is full. No more costumers are allowed to enter. Client is leaving the store.\n", clientID);
@@ -100,29 +89,14 @@ void client_is_leaving(int clientID) {
 void* f_costumer (void *v) {
 	int clientID = *(int *) v;
 	sem_wait(&mutex);
-	if (num_clients >= num_total_customers) {
+	if (num_clients >= num_max_costumers) {
 		sem_post(&mutex);
 		shop_is_full(clientID);
 	}
 	++num_clients;
 	sem_post(&mutex);
 
-	/*
-	if(isFull(q_sofa)==0){
-		sem_post(&mutex);
-		//sem_wait(&queue_wr);
-		sem_wait(&sofa_empty);
-		//sem_wait(&queue_wr);
-		going_to_the_sofa(clientID);
-		enqueue(q_sofa,clientID);
-		//down_queue(&sofa_full, &sofa_empty);
-		
-		//sem_post(&queue_wr);
 
-	}else{
-		sem_post(&mutex);
-		*/
-		//printf("AQUI\n");
 	sem_wait(&waiting_room_empty);
 	//sem_wait(&queue_wr);
 	sem_wait(&queue_wr);
@@ -190,31 +164,34 @@ void* f_barber (void *v) {
 /*
  * Checks if the number of command line arguments is correct. The first argument is always the program name
  */
-void check_arguments(int argc, char* argv[]) {
+int check_arguments(int argc, char* argv[]) {
 	if (argc != 5) {
-		printf("Usage: ./<number of processes> <number of ressources> <number of instances per ressource>\n");
+		//printf("Usage: ./<number of processes> <number of ressources> <number of instances per ressource>\n");
 
-		exit(0);
+		//exit(0);
+		return 0;
 	}
+	return 1;
 }
 
 
 int main(int argc, char* argv[]) {
-	/*check_arguments(argc, argv);
+	if(check_arguments(argc, argv)){
 
-	num_chairs = atoi(argv[1]);
-	num_barbers = atoi(argv[2]);
-	num_seats_sofa = atoi(argv[3]);
-	num_total_customers = atoi(argv[4]);*/
+		num_chairs = atoi(argv[1]);
+		num_barbers = atoi(argv[2]);
+		num_seats_sofa = atoi(argv[3]);
+		num_max_costumers = atoi(argv[4]);
+	}	
     srand( (unsigned)time(NULL) );
-	thr_costumers = (pthread_t *)malloc(sizeof(pthread_t) * num_total_customers);
+	thr_costumers = (pthread_t *)malloc(sizeof(pthread_t) * num_max_costumers);
 	thr_barbers = (pthread_t *)malloc(sizeof(pthread_t) * num_barbers);
 	
-	q_waiting_room = createQueue(num_total_customers-num_seats_sofa);
+	q_waiting_room = createQueue(num_max_costumers-num_seats_sofa);
 	q_sofa = createQueue(num_seats_sofa);
-	//q_payment = createQueue(num_total_customers);
+	//q_payment = createQueue(num_max_costumers);
 
-	int client[num_total_customers];
+	int client[num_max_costumers];
 
 	sem_init(&mutex, 0, 1);
 	sem_init(&chair, 0, num_chairs);
@@ -225,9 +202,7 @@ int main(int argc, char* argv[]) {
     sem_init(&queue_wr,0,1);
     sem_init(&queue_s,0,1);
     sem_init(&queue_c,0,1);
-	sem_init(&waiting_room_full, 0, 0);
-	sem_init(&waiting_room_empty, 0, num_total_customers - num_seats_sofa);
-	sem_init(&sofa_full, 0, 0);
+	sem_init(&waiting_room_empty, 0, num_max_costumers - num_seats_sofa);
 	sem_init(&sofa_empty, 0, num_seats_sofa);
 
 	int i = 0;
@@ -236,7 +211,7 @@ int main(int argc, char* argv[]) {
     int shuffle[num_barbers];
 
 
-	for (i = 0; i < num_total_customers; i++) {
+	for (i = 0; i < num_max_costumers; i++) {
 		client[i] = i;
 
 		pthread_create(&thr_costumers[i], 0, f_costumer, &client[i]);
@@ -246,11 +221,11 @@ int main(int argc, char* argv[]) {
         shuffle[i]= al+i;
 		pthread_create(&thr_barbers[i], 0, f_barber, &shuffle[i]);
 	}
-    
-    while (tranks_clients<num_total_customers)
-    {
-        sleep (1);
-    }
+    //sleep(11000);
+    for (i = 0; i < num_max_costumers; i++) {
+
+		pthread_join(thr_costumers[i], NULL);
+	}
 	
 
 	free(thr_costumers);
